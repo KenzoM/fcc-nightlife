03dbddaac0d1cb11801bd41314832c8801133a65
Kenzo Mendoza <kenzom954@gmail.com>
2017-01-10 22:53:44 -0800

    Added About section

--- src/client/components/About.js
+++ src/client/components/About.js
@@ -3,7 +3,20 @@ import React, { Component } from 'react'
 class About extends Component {
   render(){
     return(
-      <div>About</div>
+      <div className="title">
+        <h1>About the Nightlife Coordination App</h1>
+        <h3>
+          A <a href="https://www.freecodecamp.com/challenges/build-a-nightlife-coordination-app">Free Code Camp Project</a>&nbsp;
+          that utilizes single-page React/Redux app with Node/Express on the back end.
+          <br />
+          <br />
+          <a href="https://github.com/Neotriz/fcc-nightlife">View Source Code on GitHub</a>
+          <br />
+          <br />
+          <a href="http://kenzomendoza.com/">Home Portfolio</a>
+        </h3>
+
+      </div>
     )
   }
 }
