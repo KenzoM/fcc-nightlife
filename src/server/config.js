7a3847d87195e682a7884356bfffdfcb1d7aa508
Kenzo Mendoza <kenzom954@gmail.com>
2017-01-13 12:23:32 -0800

    updated config.js

--- src/server/config.js
+++
@@ -1,8 +0,0 @@
-module.exports ={
-  secret: 'd0829r8sdifdkvjdlakfaWQ!??SDFsdfsdjf=vcuzhhhhh',
-  consumer_key: 'VWr9fLWUAVVysF6a0n5ETg',
-  consumer_secret: 'uiRR3XetMsz4QKxOfrOMMovhjag',
-  token: '8ySZ933jN55vudN21_ZTvLYIYEXq-_ar',
-  token_secret: 'wKn5_Ykn5KIOS716leP76zhMUGs',
-  googleAPI: 'AIzaSyACO9SDgAwetIO3Zxl3tXQGMR90sCrhQHM'
-}
